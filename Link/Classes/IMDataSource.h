b42cdb6b669cfc37a703c4b4c1c505c0531b17d3
sChao001 <948994708@qq.com>
2018-04-20 13:37:12 +0800

    Imdatasource

---
+++ Link/Classes/IMDataSource.h
@@ -0,0 +1,13 @@
+//
+//  IMDataSource.h
+//  Link
+//
+//  Created by Surdot on 2018/4/20.
+//  Copyright © 2018年 Surdot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IMDataSource : NSObject
+
+@end
---
+++ Link/Classes/IMDataSource.m
@@ -0,0 +1,13 @@
+//
+//  IMDataSource.m
+//  Link
+//
+//  Created by Surdot on 2018/4/20.
+//  Copyright © 2018年 Surdot. All rights reserved.
+//
+
+#import "IMDataSource.h"
+
+@implementation IMDataSource
+
+@end
